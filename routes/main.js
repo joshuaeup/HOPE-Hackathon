76336413027f07426d2c1094e32eab2d8466d68d
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2020-12-15 00:02:58 -0500

    Added email functionality

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const data = require("../data.json");
+const nodemailer = require("nodemailer");
+const credentials = require("../config/pass");
+
+// Create mail profile using smtp gmail server
+const profile = {
+    host: "smtp.gmail.com",
+    auth: {
+        // Enter login credentials into authentication object
+        user: credentials.USER,
+        pass: credentials.PASS,
+    },
+};
+// Create email transporter using nodemailer
+const transporter = nodemailer.createTransport(profile);
+
+// Verify is active without error
+transporter.verify((err, res) => {
+    if (err) console.log(err);
+    else console.log("Mail Server running");
+});
 
 // Home route
 router.get("/", (req, res) => {
@@ -16,6 +36,29 @@ router.get("/:object", (req, res) => {
     }
     // Send result to page
     res.render("info", { data: data[item] });
+});
+
+router.post("/:object", (req, res) => {
+    const item = req.params.object;
+    const email = req.body.email;
+
+    const mail = {
+        from: "Hope",
+        to: email,
+        subject: `Tips related to your ${item.toString()}`,
+        text: data[item][0].title,
+    };
+
+    // Send email with mail object passed in then check for success
+    transporter.sendMail(mail, (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(res);
+        }
+    });
+
+    res.render("home");
 });
 
 module.exports = router;
