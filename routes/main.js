24e9c8a3d8ddf61c95e9cc11b5ce37fcf0a59710
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2020-12-14 22:39:55 -0500

    Added route function and send json data view

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const data = require("../data.json");
 
 // Home route
 router.get("/", (req, res) => {
     res.render("home");
+});
+
+router.get("/:object", (req, res) => {
+    const item = req.params.object;
+    // check if req exists
+    if (!data[item]) {
+        // Set status code to not found and display message
+        return res.status(404).send("Object cannot be found");
+    }
+    // Send result to page
+    res.render("info", { data: data[item] });
 });
 
 module.exports = router;
