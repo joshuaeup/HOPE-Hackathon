f3ece11bf25b6c5c0d764b986f53fbd8ad3bb984
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2020-12-16 15:04:12 -0500

    Added Gif api

--- routes/main.js
+++ routes/main.js
@@ -7,6 +7,7 @@ const transporter = require("../config/transporter");
 const request = require("request");
 const { json } = require("body-parser");
 const apiKey = "decf8780916542018d312ec3ef91935b";
+const gifApiKey = "dc6zaTOxFJmzC";
 
 // Home route
 router.get("/", (req, res) => {
@@ -19,6 +20,22 @@ router.get("/api", (req, res) => {
 
 // Window has a higher specificity to run this code on the window route instead of the :object route
 router.get("/window", (req, res) => {
+    const object = "window";
+    const collectedGif = "";
+    request(
+        `http://api.giphy.com/v1/gifs/search?q=${object}&limit=1&api_key=${gifApiKey}`,
+        (err, response, body) => {
+            if (err) {
+                res.render("info", { gif: null, error: "Gif Not Found" });
+            } else {
+                // let gif = response.data[0].images.fixed_height.url;
+                // console.log(response);
+                const gif = JSON.parse(body).data[0].images.original.url;
+                collectedGif = gif;
+            }
+        }
+    );
+
     // API info
     const city = "Charlotte";
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
@@ -42,6 +59,7 @@ router.get("/window", (req, res) => {
             console.log(weatherText);
             res.render("windowInfo", {
                 weather: weatherText,
+                gif: collectedGif,
                 data: data.window,
                 error: null,
             });
@@ -52,13 +70,27 @@ router.get("/window", (req, res) => {
 // Retrieves data from param if exist
 router.get("/:object", (req, res) => {
     const item = req.params.object;
+
     // check if req exists
     if (!data[item]) {
         // Set status code to not found and display message
         return res.status(404).send("Object cannot be found");
     }
-    // Send result to page
-    res.render("info", { data: data[item] });
+
+    request(
+        `http://api.giphy.com/v1/gifs/search?q=${item}&limit=1&api_key=${gifApiKey}`,
+        (err, response, body) => {
+            if (err) {
+                res.render("info", { gif: null, error: "Gif Not Found" });
+            } else {
+                // let gif = response.data[0].images.fixed_height.url;
+                // console.log(response);
+                const gif = JSON.parse(body).data[0].images.original.url;
+                // Send result to page
+                res.render("info", { data: data[item], gif: gif });
+            }
+        }
+    );
 });
 
 // Post method for email
